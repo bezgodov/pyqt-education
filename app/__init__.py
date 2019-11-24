d903d663f68a98c09d18c8f8f9cafdadef5b57f2
Max Bezgodov <maksim_bezgodov@mail.ru>
2019-11-25 09:50:58 +1000

    Include exit action
    Which doesn't work on Mac

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
+from PyQt5.QtWidgets import QAction, qApp
+
 from app.views.Tabs import Tabs
 
 class App():
     def __init__(self, parent):
+        self.init_exit_action(parent)
+
         _tabs = Tabs(parent)
         _tabs.make_tabs()
+
+    def init_exit_action(self, parent):
+        exit_action = QAction('&Exit', parent)
+        exit_action.setShortcut('Ctrl+C')
+        exit_action.triggered.connect(qApp.quit)
