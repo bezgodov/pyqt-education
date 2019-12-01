2166c869fa09c9418c05b68b1e8c59525719de94
Max Bezgodov <maksim_bezgodov@mail.ru>
2019-12-01 15:33:50 +1000

    Remove obsolete class QtableWidgetCustom
    Make Table view extending QTableWidget

--- app/views/Table.py
+++ app/views/Table.py
@@ -4,76 +4,14 @@ from PyQt5.QtCore import Qt
 from app.models.Database import Database
 from app.models.Store import Store
 
-class Table(QWidget):
+class Table(QTableWidget):
 
     PERCENT_TO_START_LOADING = 0.9
 
     def __init__(self):
-        QWidget.__init__(self)
-        self.setMinimumSize(640, 980)
-
-    def generate_table(self, headers, rows):
-        table = self.init_table(headers, rows)
-
-        self.set_table_headers(table, headers)
-        self.set_table_rows(table, rows)
-
-        return table
-
-    def init_table(self, headers, rows):
-        table = QTableWidgetCustom(self)
-        table.setColumnCount(len(headers))
-        table.setRowCount(len(rows))
-
-        table.setHorizontalHeaderLabels(headers)
-        table.resizeColumnsToContents()
-        table.resizeRowsToContents() # this line doesn't work actually, next one span cells to theirs content
-        table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-
-        return table
-
-    def set_table_headers(self, table, headers):
-        for i, h in enumerate(headers):
-            table.horizontalHeaderItem(i).setToolTip(h)
-            table.horizontalHeaderItem(i).setTextAlignment(Qt.AlignLeft)
-
-    def set_table_rows(self, table, rows):
-        for i, r in enumerate(rows):
-            self.set_row_columns(table, r, i)
-
-    def set_row_columns(self, table, row, rowPosition):
-        for j, c in enumerate(row):
-            table.setItem(rowPosition, j, QTableWidgetItem(str(c)))
-
-    def add_row_to_table(self, table, row):
-        rowPosition = table.rowCount()
-        table.insertRow(rowPosition)
-
-        self.set_row_columns(table, row, rowPosition)
-
-    def add_rows_to_table(self, table):
-        rows = self.get_data_to_add()
-        for r in rows:
-            self.add_row_to_table(table, r)
-
-    def get_data_to_add(self):
-        db = Database()
-
-        current_tab = Store.get_current_tab()
-
-        last_row = current_tab['last_row_loaded']
-        last_row_loaded = last_row + Database.ROWS_PER_LOAD
-
-        data = db.get_table_data(current_tab['title'], last_row)
-
-        Store.set_last_row(Store.get_current_tab(), last_row_loaded)
-
-        return data['rows']
-
-class QTableWidgetCustom(QTableWidget):
-    def __init__(self, table):
         QTableWidget.__init__(self)
-        self.table = table
+        self.db = Database()
+        self.setMinimumSize(640, 480)
 
     def wheelEvent(self, evt):
         super().wheelEvent(evt)
@@ -81,5 +19,63 @@ class QTableWidgetCustom(QTableWidget):
         current = scrollbar.value()
         maximum = scrollbar.maximum()
 
-        if current > maximum * self.table.PERCENT_TO_START_LOADING:
-            self.table.add_rows_to_table(self)
+        if current > maximum * self.PERCENT_TO_START_LOADING:
+            self.add_rows_to_table()
+
+    def generate_table(self, headers, rows):
+        self.init_table(headers, rows)
+
+        self.set_table_headers(headers)
+        self.set_table_rows(rows)
+
+        return self
+
+    def init_table(self, headers, rows):
+        self.setColumnCount(len(headers))
+        self.setRowCount(len(rows))
+
+        self.setHorizontalHeaderLabels(headers)
+        self.resizeColumnsToContents()
+        self.resizeRowsToContents() # this line doesn't work actually, next one span cells to theirs content
+        self.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
+
+        return self
+
+    def set_table_headers(self, headers):
+        for i, h in enumerate(headers):
+            self.horizontalHeaderItem(i).setToolTip(h)
+            self.horizontalHeaderItem(i).setTextAlignment(Qt.AlignLeft)
+
+    def set_table_rows(self, rows):
+        for i, r in enumerate(rows):
+            self.set_row_columns(r, i)
+
+    def set_row_columns(self, row, rowPosition):
+        for j, c in enumerate(row):
+            item = QTableWidgetItem(str(c))
+            if (j == 0):
+                item.setFlags( Qt.ItemIsSelectable |  Qt.ItemIsEnabled )
+            self.setItem(rowPosition, j, item)
+
+    def add_row_to_table(self, row):
+        rowPosition = self.rowCount()
+        self.insertRow(rowPosition)
+
+        self.set_row_columns(row, rowPosition)
+
+    def add_rows_to_table(self):
+        rows = self.get_data_to_add()
+        for r in rows:
+            self.add_row_to_table(r)
+
+    def get_data_to_add(self):
+        current_tab = Store.get_current_tab()
+
+        last_row = current_tab['last_row_loaded']
+        last_row_loaded = last_row + Database.ROWS_PER_LOAD
+
+        data = self.db.get_table_data(current_tab['title'], last_row)
+
+        Store.set_last_row(Store.get_current_tab(), last_row_loaded)
+
+        return data['rows']
